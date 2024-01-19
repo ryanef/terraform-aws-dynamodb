77732e93a3ea0d0ad0076caa974b0a10e2732a20
ryan-wsl <ryanef39@gmail.com>
2024-01-19 18:56:55 -0500

    first

---
+++ main.tf
@@ -0,0 +1,28 @@
+resource "aws_dynamodb_table" "table" {
+  name           = "${var.table_name}-${var.environment}"
+  billing_mode   = var.billing_mode
+  hash_key       = var.hash_key
+  range_key      = var.range_key
+
+  attribute {
+    name = var.hash_key
+    type = "S"
+  }
+  attribute {
+    name = var.range_key
+    type = "S"
+  }
+
+  ttl {
+    attribute_name = "TimeToExist"
+    enabled        = var.ttl_enabled
+  }
+
+
+
+  tags = {
+    Name        = "${var.table_name}-${var.environment}"
+   
+  }
+}
+
---
+++ variables.tf
@@ -0,0 +1,37 @@
+variable "table_name" {
+  default = "ddbtable"
+  type = string
+}
+
+variable "billing_mode" {
+  default = "PAY_PER_REQUEST"
+  type=string
+}
+
+variable "hash_key" {
+  default = "pk"
+  type = string
+}
+
+
+variable "range_key" {
+  default = "sk"
+  type = string
+}
+
+
+
+variable "attribute_type"{
+  default = "S"
+}
+
+variable "ttl_enabled" {
+  default = false
+  type = bool
+}
+
+variable "environment" {
+  default = "dev"
+  type = string
+}
+
